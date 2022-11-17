b1f4aec987b19e9806776dd13844fd4ca5125d04
valefe <valentina.fajardoa@utadeo.edu.co>
2022-11-17 03:43:40 -0500

    login y recuperacion de contraseña listos

--- app/src/main/java/valentina/fajardo/scuritnova/LoginActivity.java
+++ app/src/main/java/valentina/fajardo/scuritnova/LoginActivity.java
@@ -69,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(), "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
                             }else{
                                 limpiar();
-                                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                                Intent i = new Intent(LoginActivity.this, MapsActivity.class);
                                 startActivity(i);
                             }
                             alerta.dismiss();
